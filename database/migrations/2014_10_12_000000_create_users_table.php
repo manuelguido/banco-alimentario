b63240461412f548830adb42c0ebea3c8a21ebba
manuelguido <manuelguido.m@gmail.com>
2020-05-12 14:14:23 -0300

    user migration reestructure

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,12 +17,10 @@ class CreateUsersTable extends Migration
             $table->increments('id');
             $table->string('name');
             $table->string('email')->unique();
-            $table->bigInteger('dni');
             $table->bigInteger('phone');
+            $table->bigInteger('dni');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('rol');
-            $table->boolean('isActive')->default(0);
             $table->rememberToken();
             $table->timestamps();
         });
