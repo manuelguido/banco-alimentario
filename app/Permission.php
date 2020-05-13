d97e88f965d59269b399b8a483488c4eb6dcd888
manuelguido <manuelguido.m@gmail.com>
2020-05-13 12:24:28 -0300

    permission roles

--- app/Permission.php
+++ app/Permission.php
@@ -6,5 +6,22 @@ use Illuminate\Database\Eloquent\Model;
 
 class Permission extends Model
 {
-    //
+    /**
+    * Attributes
+    */
+    protected $table = 'permissions';
+
+    protected $primaryKey = 'id';
+
+    protected $fillable = [
+        'name',
+    ];
+
+    public $timestamps = false;
+
+    public function roles()
+    {
+        return $this->hasMany('App\Role', 'permission_role', 'permission_id')->get();
+    }
+
 }
