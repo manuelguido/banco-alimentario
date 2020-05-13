a9aa93ad623623cd50a85b5a14da531b6c3c467b
manuelguido <manuelguido.m@gmail.com>
2020-05-13 15:21:18 -0300

    Neightborhood model

--- app/Neighborhood.php
+++ app/Neighborhood.php
@@ -6,7 +6,28 @@ use Illuminate\Database\Eloquent\Model;
 
 class Neighborhood extends Model
 {
+    
+    /**
+    * Attributes
+    */
+    protected $table = 'neighborhoods';
+
+    protected $primaryKey = 'id';
+
     protected $fillable = [
-        'neighborhood_id', 'name',
+        'name',
     ];
+
+    public $timestamps = false;
+
+    public function giverAddresses()
+    {
+        return $this->hasMany('App\GiverAddress', 'neighborhood_id')->get();
+    }
+
+    public function organizationAddresses()
+    {
+        return $this->hasMany('App\OrganizationAddress', 'neighborhood_id')->get();
+    }
+
 }
