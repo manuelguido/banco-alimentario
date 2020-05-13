59b586b874dd36f155ad63210e645dca08e015af
manuelguido <manuelguido.m@gmail.com>
2020-05-13 15:23:16 -0300

    type and category models

---
+++ app/Category.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    /**
+    * Attributes
+    */
+    protected $table = 'categories';
+
+    protected $primaryKey = 'id';
+
+    protected $fillable = [
+        'name',
+    ];
+
+    public $timestamps = false;
+
+    public function products()
+    {
+        return $this->hasMany('App\Product', 'category_id')->get();
+    }
+
+    public function items()
+    {
+        return $this->hasMany('App\Item', 'category_id')->get();
+    }
+    
+}
---
+++ app/Type.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Type extends Model
+{
+    /**
+    * Attributes
+    */
+    protected $table = 'types';
+
+    protected $primaryKey = 'id';
+
+    protected $fillable = [
+        'name',
+    ];
+
+    public $timestamps = false;
+
+    public function products()
+    {
+        return $this->hasMany('App\Product', 'type_id')->get();
+    }
+
+    public function items()
+    {
+        return $this->hasMany('App\Item', 'type_id')->get();
+    }
+}
