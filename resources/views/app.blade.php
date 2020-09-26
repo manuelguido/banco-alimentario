42ab0cd7e913316eebdda8e29bfb47fa725eb04b
manuelguido <manuelguido.m@gmail.com>
2020-09-26 18:18:26 -0300

    updated files for deploy

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -13,6 +13,6 @@
     <div id="app">
         <app></app>
     </div>
-    <script src="{{ asset('js/app.js') }}"></script>
+    <script src="{{ asset('js/app.js', true) }}"></script>
 </body>
 </html>
