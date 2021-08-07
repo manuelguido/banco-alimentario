feb5052aff4f763613072f7448843362f93eff58
“manuelguido” <manuelguido.m@gmail.com>
2021-08-07 00:38:44 -0300

    eliminé: vistas en js

--- resources/js/views/dashboard/Donations.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="container-fluid">
-    <div class="row">
-      <div class="col-12 text-center">
-        Donaciones
-      </div>
-    </div>
-  </div>
-</template>
--- resources/js/views/dashboard/Home.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="container-fluid">
-    <div class="row">
-      <div class="col-12 text-center">
-        Inicio
-      </div>
-    </div>
-  </div>
-</template>
