0b94c1e41f231ad29fa94880641d2bed0fddc338
“manuelguido” <manuelguido.m@gmail.com>
2021-08-08 01:21:53 -0300

    middleware de permisos y roles

---
+++ app/Http/Middleware/CheckPermission.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class CheckPermission
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next, $role)
+    {
+        if(!$request->user()->hasPermission($role)) {
+            abort(401);
+        }
+        return $next($request);
+    }
+}
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,8 +1,3 @@
 const mix = require('laravel-mix');
 
-// Main resources
-
-mix.js([
-  'resources/js/app.js',
-  'resources/js/custom/lazysizes.min.js',
-  ], 'public/js/app01.min.js').sourceMaps();
+mix.js('resources/js/app.js', 'public/js/app.min.js').sourceMaps();
